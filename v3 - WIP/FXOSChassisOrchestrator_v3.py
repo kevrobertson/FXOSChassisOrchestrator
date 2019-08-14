13190b25267970eb49cbc2ae8b8a9f8e929dbc3c
Kevin Robertson <50793183+kevrobertson@users.noreply.github.com>
2019-08-14 14:47:47 +0300

    SNMP Properties added

--- v3 - WIP/FXOSChassisOrchestrator_v3.py
+++ v3 - WIP/FXOSChassisOrchestrator_v3.py
@@ -1,6 +1,5 @@
 import xlrd
 import sys
-from xlrd.sheet import ctype_text
 
 sys.stdout = open("FXOS_Config.txt", "w")
 
@@ -11,18 +10,40 @@ def dns_servers(localFunction):
     return 1
 
 def ntp_servers(localFunction):
-    print ("scope system")
-    print (" scope services")
-    print ("  create ntp-server %s" % localFunction[1])
+    print("scope system")
+    print(" scope services")
+    print("  create ntp-server %s" % localFunction[1])
     if localFunction[2] != '':
-        print ("  set ntp-sha1-key-string %s" % int(localFunction[2]))
-        print ("%s" % localFunction[3])
-        print ("  exit")
-        print (" enable ntp-authentication")
+        print("  set ntp-sha1-key-string %s" % int(localFunction[2]))
+        print("%s" % localFunction[3])
+        print("  exit")
+        print(" enable ntp-authentication")
 
+def snmp_properties(localFunction):
+    print("scope monitoring")
+    print(" enable snmp")
+    print(" set snmp syscontact %s" % localFunction[3])
+    print(" set snmp syslocation %s" % localFunction[4])
+    if localFunction[1] != 'v3':
+        print(" set snmp community")
+        print("%s" % localFunction[2])
+    else:
+        print(" create snmp-user %s" % localFunction[5])
+        print("%s" % localFunction[6])
+        print(" set aes-128 %s" % localFunction[7])
+        print(" set priv-password")
+        print("%s" % localFunction[8])
+        print("%s" % localFunction[8])
+
+def snmp_traps(localFunction):
+    print("scope monitoring")
+    print(" enable snmp")
+    print(" create snmp-trap %s" % localFunction[1])
 dynDispatch = {
     'dns': dns_servers,
-    'ntp': ntp_servers
+    'ntp': ntp_servers,
+    'snmp': snmp_properties,
+    'snmp trap': snmp_traps
 }
 
 wb = xlrd.open_workbook('Spreadsheet.xlsx')
@@ -34,3 +55,4 @@ for i, ws in enumerate(wb.sheets()):
         callThis = rv[0].lower()
         if callThis in dynDispatch:
             dynDispatch[callThis](rv)
+
